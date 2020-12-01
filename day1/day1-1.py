0664c5c2b5ae394c8fe74549e7e39c40ddcfdd7a
Brandon Wood <bwood@gci.com>
2020-12-01 09:17:28 -0900

    day 1 solution. This is a pretty brute-force method, but it works.

---
+++ day1/day1-1.py
@@ -0,0 +1,10 @@
+file = open("day1/input.txt", "r")
+lines = file.readlines()
+
+for i in range(len(lines)):
+    for j in range(i + 1, len(lines)):
+        val1 = int(lines[i])
+        val2 = int(lines[j])
+        if (val1 + val2 == 2020):
+            print(val1 * val2)
+            quit()
---
+++ day1/day1-2.py
@@ -0,0 +1,12 @@
+file = open("day1/input.txt", "r")
+lines = file.readlines()
+
+for i in range(len(lines)):
+    for j in range(i + 1, len(lines)):
+        for k in range(j + 1, len(lines)):
+            val1 = int(lines[i])
+            val2 = int(lines[j])
+            val3 = int(lines[k])
+            if (val1 + val2 + val3 == 2020):
+                print(val1 * val2 * val3)
+                quit()
